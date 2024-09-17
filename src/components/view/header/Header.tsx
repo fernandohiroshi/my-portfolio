d4985cd163884b1e7585dca93139f44ed21d29ad
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-17 01:46:25 -0300

    feat: create Header section

---
+++ src/components/view/header/Header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Nav } from './nav/Nav'
+
+export function Header() {
+  return (
+    <header className="mx-auto flex max-w-4xl items-center justify-between px-2 py-4">
+      <Link href="/#" title="Home">
+        <Image
+          src="https://imgix.cosmicjs.com/503b4b60-749f-11ef-98d9-cbcb72f3073e-logo.png"
+          alt="Logo"
+          width={40}
+          height={40}
+        />
+      </Link>
+      <Nav />
+    </header>
+  )
+}
