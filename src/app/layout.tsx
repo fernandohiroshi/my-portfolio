b2fbc751340a0937e1fc335ca137594f6267205b
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-17 01:45:46 -0300

    feat: add icon and Header component; align layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
-import type { Metadata } from 'next'
-
 import './globals.css'
 import { Raleway } from 'next/font/google'
+import { Metadata } from 'next/types'
 
 import { ThemeProvider } from '@/components/theme/provider'
+import { Header } from '@/components/view/header/Header'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fernando Hiroshi Portfolio',
   description: '',
+  icons: {
+    icon: 'https://imgix.cosmicjs.com/503b4b60-749f-11ef-98d9-cbcb72f3073e-logo.png',
+  },
 }
 
 export default function RootLayout({
@@ -20,7 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${raleway.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <Header />
           {children}
         </ThemeProvider>
       </body>
