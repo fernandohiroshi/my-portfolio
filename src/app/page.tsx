8c42658a93846f3840930ac12c1e699bdd0bcf28
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-18 20:05:06 -0300

    feat: create Avatar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from '@/components/theme/ThemeToggle'
+import Avatar from '@/components/view/avatar/Avatar'
 import Feature from '@/components/view/feature/Feature'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <main className="min-h-screen px-4">
         <div className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4">
           <Feature />
+          <Avatar />
         </div>
       </main>
       <div className="fixed bottom-4 right-4">
