a606c47ae8108a7e247ae1cc597289d27918de36
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-18 19:27:46 -0300

    feat: create Box component for bentobox grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { ThemeToggle } from '@/components/theme/ThemeToggle'
+import Feature from '@/components/view/feature/Feature'
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <main className="min-h-screen px-4">
+        <div className="mx-auto grid max-w-5xl grid-flow-dense grid-cols-12 gap-4">
+          <Feature />
+        </div>
+      </main>
       <div className="fixed bottom-4 right-4">
         <ThemeToggle />
       </div>
-    </main>
+    </>
   )
 }
---
+++ src/components/view/bento/Box.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { motion, MotionProps } from 'framer-motion'
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+  className?: string
+} & MotionProps
+
+const Box = ({ className, ...rest }: Props) => {
+  return (
+    <motion.div
+      className={twMerge(
+        'col-span-4 min-h-[20rem] rounded-lg border bg-neutral-100 p-6 dark:bg-transparent',
+        className,
+      )}
+      {...rest}
+    />
+  )
+}
+
+export default Box
