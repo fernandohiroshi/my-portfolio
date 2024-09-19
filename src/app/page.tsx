51aa6a1af41684c3c63dba36be7a60f5dc494478
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-19 01:47:04 -0300

    feat: create Languages section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/view/about/About'
 import Animation from '@/components/view/animation/Animation'
 import Avatar from '@/components/view/avatar/Avatar'
 import Feature from '@/components/view/feature/Feature'
+import { Languages } from '@/components/view/languages/Languages'
 import ProjectButton from '@/components/view/projectButton/ProjectButton'
 import Skills from '@/components/view/skills/Skills'
 
@@ -17,6 +18,7 @@ export default function Home() {
           <About />
           <Skills />
           <ProjectButton />
+          <Languages />
         </div>
       </main>
       <div className="fixed bottom-4 right-4">
