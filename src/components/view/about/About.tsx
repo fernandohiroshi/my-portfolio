08d8a96f2a9d8a5d782cb488008ea2aac3164d57
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-18 21:59:38 -0300

    style: update text size

--- src/components/view/about/About.tsx
+++ src/components/view/about/About.tsx
@@ -3,7 +3,7 @@ import Box from '../bento/Box'
 const About = () => {
   return (
     <Box className="col-span-12 flex flex-col justify-evenly gap-4 duration-500 ease-in-out hover:text-amber-500 md:col-span-8 lg:gap-0">
-      <h2 className="text-2xl font-medium tracking-widest">About me:</h2>
+      <h2 className="text-xl font-medium tracking-widest md:text-2xl">About me:</h2>
       <p className="text-justify text-xs text-foreground opacity-80 md:text-sm">
         Hello, I&apos;m Fernando Hiroshi (武田広司) I&apos;m a web developer with a focus on front-end, with expertise
         in TypeScript, React, Next.js, Tailwind, and Sass. I have advanced proficiency in Japanese, with over 10 years
