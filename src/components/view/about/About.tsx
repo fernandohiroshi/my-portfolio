a617643a8403f0f2ab863b26c1e93c43d2e6ac91
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-20 04:42:24 -0300

    chore: update About section text

--- src/components/view/about/About.tsx
+++ src/components/view/about/About.tsx
@@ -5,13 +5,13 @@ const About = () => {
     <Box className="col-span-12 flex flex-col justify-evenly gap-4 duration-500 ease-in-out hover:text-amber-500 md:col-span-8 lg:gap-0">
       <h2 className="text-xl font-medium tracking-widest md:text-2xl">About me:</h2>
       <p className="text-justify text-xs text-foreground opacity-80 md:text-sm">
-        Hello, I&apos;m Fernando Hiroshi (武田広司) I&apos;m a web developer with a focus on front-end, with expertise
-        in TypeScript, React, Next.js, Tailwind, and Sass. I have advanced proficiency in Japanese, with over 10 years
-        of experience living in Japan, providing a multicultural perspective and adaptability.
+        Hello, I&apos;m Fernando Hiroshi, a web developer with over 3 years of experience, focused on front-end
+        development with expertise in TypeScript, React, Next.js, and Tailwind CSS. I founded Konbini Web, a company
+        that creates modern and unique websites with an emphasis on design and functionality.
         <br /> <br />
-        Technical proficiency in English: basic reading/writing. Graduated from the Front-End Engineer course at the
-        British School of Arts and Technology (EBAC). Additional knowledge in React Native, Node.js, Redux, Axios,
-        Docker, Cypress testing, and currently improving skills in MongoDB, Prisma, and AWS. 🇧🇷 🇯🇵 🇺🇸
+        With over 10 years of experience living in Japan, I bring a multicultural perspective and adaptability. I
+        graduated from the Front-End Engineer course at the British School of Arts and Technology (EBAC) and continue to
+        expand my knowledge, focusing on delivering innovative web solutions.
       </p>
     </Box>
   )
