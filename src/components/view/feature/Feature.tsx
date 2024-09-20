6afc60d46d610f468c2c8a5acfbce764f4a96cc7
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-20 04:42:55 -0300

    chore: update Feature component subtitle

--- src/components/view/feature/Feature.tsx
+++ src/components/view/feature/Feature.tsx
@@ -9,9 +9,10 @@ const Feature = () => {
         <span className="text-pink-500">Hello,</span> my name is Hirøshi <br />
         <span className="text-purple-500">How</span> are you?
       </h1>
-      <p className="text-start text-xs opacity-80 md:text-sm">
-        I&apos;m a web developer, with expertise on React, Next.js, TypeScript, Tailwind CSS.
-      </p>
+      <a href="https://konbini-code.vercel.app/pt" target="_blank" className="text-start text-xs opacity-80 md:text-sm">
+        I create modern websites with unique designs at{' '}
+        <span className="animate-pulse font-medium underline">Konbini Web</span> .
+      </a>
       <div className="flex items-center justify-end gap-4">
         <Button variant="outline">Github</Button>
         <Button variant="outline">Linkedin</Button>
