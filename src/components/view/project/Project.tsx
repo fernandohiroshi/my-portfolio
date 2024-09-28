57e9be33015371c08ad80bac4c24ab2537cc1143
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-09-28 04:05:19 -0300

    feat: create Project component for Projects page

---
+++ src/components/view/project/Project.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { useState } from 'react'
+
+import { Lens } from '@/components/ui/aceternity/lens'
+import { Button } from '@/components/ui/shadcn/button'
+import Box from '@/components/view/bento/Box'
+
+const projects = [
+  {
+    img: 'https://imgix.cosmicjs.com/181e4300-748a-11ef-98d9-cbcb72f3073e-openGraph.jpg',
+    name: 'Konbini Code',
+    code: '#',
+    live: 'https://konbini-code.vercel.app/en',
+  },
+]
+
+const Project = () => {
+  const [hoveringIndex, setHoveringIndex] = useState<number | null>(null)
+
+  return (
+    <>
+      {projects.map((p, index) => (
+        <Box key={index} className="col-span-12 md:col-span-6">
+          <Lens
+            hovering={hoveringIndex === index}
+            setHovering={(isHovering) => setHoveringIndex(isHovering ? index : null)}
+          >
+            <Image
+              src={p.img}
+              alt="image"
+              width={500}
+              height={500}
+              className="h-full w-full rounded-2xl object-cover"
+            />
+          </Lens>
+          <motion.div
+            animate={{
+              filter: hoveringIndex === index ? 'blur(2px)' : 'blur(0px)',
+            }}
+            className="mt-4"
+          >
+            <div className="flex items-center justify-between">
+              <h2 className="text-2xl">{p.name}</h2>
+              <div className="flex items-center gap-4">
+                <a href={p.code}>
+                  <Button variant="outline" size="sm">
+                    Code
+                  </Button>
+                </a>
+                <a href={p.live}>
+                  <Button variant="outline" size="sm">
+                    Live
+                  </Button>
+                </a>
+              </div>
+            </div>
+          </motion.div>
+        </Box>
+      ))}
+    </>
+  )
+}
+
+export default Project
