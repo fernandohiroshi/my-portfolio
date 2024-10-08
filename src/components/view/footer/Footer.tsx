a42189f0daa837ccde6b7439e2e3eb14ec2fb2ea
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-10-08 04:16:02 -0300

    fix(Footer): removed h2 and PiArrowCircleUpRightLight color edit

--- src/components/view/footer/Footer.tsx
+++ src/components/view/footer/Footer.tsx
@@ -8,23 +8,11 @@ import Box from '../bento/Box'
 const Footer = () => {
   return (
     <Box className="relative col-span-12 min-h-[14rem] border-none bg-gradient-to-b from-background to-neutral-300 dark:to-neutral-900">
-      <h2 className="flex w-full items-center justify-between text-2xl font-light uppercase tracking-widest duration-500 ease-in-out md:text-4xl lg:text-5xl">
-        <span className="animate-bounce">H</span>
-        <span className="animate-bounce delay-75">I</span>
-        <span className="animate-bounce delay-100">R</span>
-        <span className="animate-bounce text-pink-500 delay-150">
-          <div className="animate-spin">Ø</div>
-        </span>
-        <span className="animate-bounce delay-100">S</span>
-        <span className="animate-bounce delay-75">H</span>
-        <span className="animate-bounce">I</span>
-      </h2>
-
       <div className="absolute bottom-4 left-4 flex flex-col gap-4">
         <a
           href="https://konbinicode.com"
           target="_blank"
-          className="inline-flex items-center gap-2 text-xl duration-300 ease-in-out hover:font-semibold hover:brightness-150"
+          className="inline-flex items-center gap-2 text-2xl duration-300 ease-in-out hover:font-semibold hover:brightness-150"
         >
           <Image
             alt="Konbini Web"
@@ -55,7 +43,7 @@ const Footer = () => {
           href="#"
           className="inline-flex opacity-80 duration-300 ease-in-out hover:-rotate-45 hover:scale-125 hover:opacity-100"
         >
-          <PiArrowCircleUpRightLight className="animate-pulse hover:animate-none" size={52} />
+          <PiArrowCircleUpRightLight className="animate-pulse text-pink-800 hover:animate-none" size={52} />
         </a>
       </div>
     </Box>
